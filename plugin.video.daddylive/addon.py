06824e8a909656752daaaf51ac4699b0b8154748
slipsystem <andretiltman@gmail.com>
2025-01-21 22:53:35 +0200

    updated Daddylive Url
    updated DaddyLive Url

--- plugin.video.daddylive/addon.py
+++ plugin.video.daddylive/addon.py
@@ -52,7 +52,7 @@ def tv_icons():
 tv_icons_dir = tv_icons()
 mode = addon.getSetting('mode')
 baseurl = 'https://thedaddy.to/'
-json_url = f'{baseurl}stream/stream-%s.php'
+json_url = '{baseurl}stream/stream-%s.php'
 schedule_url = baseurl + 'schedule/schedule-generated.json'
 UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
 FANART = addon.getAddonInfo('fanart')
